ee720cc2147cbccd1cf0a697215685c433dd9fbb
Hire Bikila <hirebikila1@gmail.com>
2025-11-09 23:27:02 +0300

    Time: 2 ms (37.25%) | Memory: 10.4 MB (87.58%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void backtrack(vector<int>& nums, vector<bool>& used, vector<int>& path, vector<vector<int>>& result) {
+    if (path.size() == nums.size()) {
+        result.push_back(path); 
+        return;
+    }
+
+    for (int i = 0; i < nums.size(); i++) {
+        if (used[i]) continue;
+        path.push_back(nums[i]);
+        used[i] = true;
+        backtrack(nums, used, path, result);
+        path.pop_back();
+        used[i] = false;
+    }
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> result;
+    vector<int> path;
+    vector<bool> used(nums.size(), false);
+    backtrack(nums, used, path, result);
+    return result;
+}
+};
